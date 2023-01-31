81bf39e2f3db64ec77ebcb83c5bb451cae92be94
Radhames Capellan Molina <radhamesc@gmail.com>
2023-01-30 22:41:58 -0500

    purchases slices

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,8 +1,16 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPurchasesThunk } from "../store/slices/purchases.slice";
 
 const Purchases = () => {
   const purchases = useSelector((state) => state.purchases);
+  const dispatch = useDispatch();
+  
+
+  useEffect(() => {
+    dispatch(getPurchasesThunk());
+  }, []);
+
   console.log(purchases);
 
   return (
--- src/store/slices/purchases.slice.jsx
+++ src/store/slices/purchases.slice.jsx
@@ -1,14 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
+import { setIsLoading } from "./isLoading.slice";
+import axios from "axios";
+import getConfig from "../../utils/getConfig";
 
 // Cambiamos mySlice por el nombre de nuestro slice (usersSlice, toDosSlice...)
 export const purchasesSlice = createSlice({
-		name: 'purchases',
-    initialState: [],
-    reducers: {
-        
-    }
-})
+  name: "purchases",
+  initialState: [],
+  reducers: {
+    setPurchases: (state, action) => {
+      const purchases = action.payload;
+      return purchases;
+    },
+  },
+});
 
-export const {  } = purchasesSlice.actions;
+export const getPurchasesThunk = () => (dispatch) => {
+  dispatch(setIsLoading(true));
+  return axios
+    .get(
+      `https://e-commerce-api-v2.academlo.tech/api/v1/purchases`,
+      getConfig()
+    )
+    .then((res) => dispatch(setPurchases(res.data)))
+    .finally(() => dispatch(setIsLoading(false)));
+};
+
+export const { setPurchases } = purchasesSlice.actions;
 
 export default purchasesSlice.reducer;
